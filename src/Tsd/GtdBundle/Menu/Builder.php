e0ef42f51e455a7cfdcea3d659349f52d033bb80
michael <Michael McAndrew>
2013-12-16 14:49:37 +0000

    simplfying menu

--- src/Tsd/GtdBundle/Menu/Builder.php
+++ src/Tsd/GtdBundle/Menu/Builder.php
@@ -15,11 +15,11 @@ class Builder extends ContainerAware
         // $menu['Actions']->addChild('Add action', array( 'route' => 'tsd_gtd_action_add'));
         $menu->addChild('Projects', array('route' => 'tsd_gtd_project_index'));
         $menu->addChild('Someday', array('route' => 'tsd_gtd_project_index', 'routeParameters' => array('timeframe' => 'someday')));
-        $menu->addChild('Add stuff', array('route' => 'tsd_gtd_stuff_add'));
         $menu->addChild('Stuff', array('route' => 'tsd_gtd_stuff_index'));
-        $menu->addChild('Process stuff', array('route' => 'tsd_gtd_stuff_process'));
-        $menu->addChild('Add context', array('route' => 'tsd_gtd_context_add'));
-        $menu->addChild('Add project tag', array('route' => 'tsd_gtd_projecttag_add'));
+        // $menu['Stuff']->addChild('Add stuff', array('route' => 'tsd_gtd_stuff_add'));
+        // $menu['Stuff']->addChild('Process stuff', array('route' => 'tsd_gtd_stuff_process'));
+        // $menu->addChild('Add context', array('route' => 'tsd_gtd_context_add'));
+        // $menu->addChild('Add project tag', array('route' => 'tsd_gtd_projecttag_add'));
         // $menu['Projects']->addChild('Add project', array( 'route' => 'tsd_gtd_project_add'));
         // $menu['Projects']->addChild('Show all', array( 'route' => 'tsd_gtd_project_index', 'routeParameters' => array('status' => 'all')));
         
